9f66e00bc2143b7c264325a6afdd25e1667c5b8e
Marcel Klehr <mklehr@gmx.net>
2015-07-21 15:29:25 +0200

    Add ui:start(opts) hook

--- client.js
+++ client.js
@@ -1,14 +1,19 @@
 var Client = require('hive-api-client')
   , page = require('page')
+  , co = require('co')
 
 module.exports = setup
-module.exports.consumes = ['auth']
+module.exports.consumes = ['hooks']
 module.exports.provides = ['ui']
 function setup(plugin, imports, register) {
-  var auth = imports.auth
+  var hooks = imports.hooks
   var ui = {
     start: function() {
-      page()
+      co(function*() {
+        var opts = {}
+        yield hooks.callHook('ui:start', opts)
+        page(opts)
+      }).then(function(){})
     },
     page: page,
     baseURL: document.location.origin // XXX: Won't work if hive is loaded in a subdir
