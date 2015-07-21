42e2de1ece7eb424059119b714de3e7c34e01ba6
Marcel Klehr <mklehr@gmx.net>
2015-07-21 14:21:07 +0200

    Begin work on client-side ui

--- client.js
+++ client.js
@@ -1,6 +1,31 @@
-var page = require('page.js')
+var Client = require('hive-api-client')
 
+module.exports = setup
+module.exports.consumes = ['auth']
+module.exports.provides = ['ui']
 function setup(plugin, imports, register) {
-
-  register()
+  var auth = imports.auth
+  var ui = {
+    start: function() {
+      var apiKey
+      if(Object.keys(auth.authentication).length) {
+        
+      }else{
+        apiKey = 'foobar'
+      }
+      var api = Client(document.location.origin, apiKey) // XXX: Will not work if hive is installed in a sub directory
+      var pathname = window.location.pathname
+        , docId = pathname.split('/')[pathname.split('/').length-1]
+      api.document.get(docId, function(er, doc) {
+        if(er) {
+          api.document.create('plaintext', cb)
+        }
+        cb(null, doc)
+        function cb(er, doc) {
+          if(er) throw er
+        }
+      })
+    }
+  }
+  register(null, {ui: ui})
 }
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+var path = require('path')
+
 module.exports = setup
 module.exports.consumes = ['assets']
 
 function setup(plugin, imports, register) {
   var assets = imports.assets
 
-  assets.registerModule(__dirname+'/client.js')
+  assets.registerModule(path.join(__dirname, 'client.js'))
 
   register()
 }
