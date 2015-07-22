b807bd2311c51112d4d72448a143b4da916e4192
Marcel Klehr <mklehr@gmx.net>
2015-07-22 14:25:52 +0200

    Moved bundle serving from hive-assets to hive-ui
    to resolve cyclic dependency

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 var path = require('path')
 
 module.exports = setup
-module.exports.consumes = ['assets']
+module.exports.consumes = ['assets', 'http', 'hooks']
 
 function setup(plugin, imports, register) {
   var assets = imports.assets
+    , http = imports.http
+    , hooks = imports.hooks
 
   assets.registerModule(path.join(__dirname, 'client.js'))
+  
+  hooks.on('http:listening', function*() {
+    http.get('/build.js', function*() {
+      if(this.cached && (yield this.cached())) return
+      this.body = yield assets.bundle()
+    })
+
+    http.get('/:id', assets.bootstrapMiddleware())
+  })
 
   register()
 }
