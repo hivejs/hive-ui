a65fe637409d6414641cffd7a6557d5ba2b6e1e5
Marcel Klehr <mklehr@gmx.net>
2015-07-30 17:12:37 +0200

    Release under the terms of GPLv3

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+/**
+ * hive.js
+ * Copyright (C) 2013-2015 Marcel Klehr <mklehr@gmx.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 var path = require('path')
 
 module.exports = setup
@@ -12,7 +29,7 @@ function setup(plugin, imports, register) {
   
   hooks.on('http:listening', function*() {
     http.get('/build.js', function*() {
-      if(this.cached && (yield this.cached())) return
+      if(yield this.cashed()) return
       this.body = yield assets.bundle()
     })
 
